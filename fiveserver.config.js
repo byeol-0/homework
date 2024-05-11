62e43c855e76de560e41a84dc098906856fb03d6
byeol-0 <wntjr5690@naver.com>
2024-05-11 23:55:46 +0900

    HTML CSS 2차 과제

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  port: 8086,
-  root: "src/",
-  open: ["index.html"],
+  port: 8080,
+  root: "naver/",
+  // open: ["index.html"],
   host: "localhost",
   browser: "chrome",
   https: false,
